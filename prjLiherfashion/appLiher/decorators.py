662b4aa64f9099de4c71474150bf3a5965f97357
carolina148c <ac176600@gmail.com>
2025-10-22 17:47:30 -0500

    Limpieza: aplico correctamente el .gitignore

--- prjLiherfashion/appLiher/decorators.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-
-def admin_required(view_func):
-    def _wrapped_view(request, *args, **kwargs):
-        if not request.user.is_authenticated or not (request.user.is_staff or request.user.is_superuser):
-            # Si no es admin → lo mandamos a la tienda
-            return redirect("pagina_principal")
-        return view_func(request, *args, **kwargs)
-    return _wrapped_view
